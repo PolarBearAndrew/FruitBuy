ec5d2832576f3c7b014d86828fe8b719bf3a2960
Andrew <chenpoanandrew@gmail.com>
2015-09-04 00:19:41 +0800

    測試

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,9 @@ router.get('/:page', (req, res, next) => {
   res.render(req.params.page);
 });
 
+router.get('/jsdc', (req, res, next) => {
+  res.render(req.params.page);
+});
+
+
 module.exports = router;
