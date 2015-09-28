759adafb546b7c19182151ceec9887e1071fbe10
Andrew <chenpoanandrew@gmail.com>
2015-09-28 11:43:46 +0800

    add cart localstorage

--- public/asset/javascripts/shop.js
+++ public/asset/javascripts/shop.js
@@ -3,6 +3,23 @@ $(document).ready( function(){
   $('html, body').on('click', '.add2cart', function(){
 
     $('#add2cart').modal('show');
+
+    var item = {
+      title: $(this).attr('data-name'),
+      url: $(this).attr('data-url'),
+      info: $(this).attr('data-info'),
+      cost: $(this).attr('data-cost'),
+      id: $(this).attr('data-id')
+    }
+
+    // console.log('add item', item);
+
+    var data = JSON.parse( localStorage.getItem("FruitBuyCart") ) || [];
+    data.push( item );
+    localStorage.setItem("FruitBuyCart", JSON.stringify(data) );
+
+    // console.log('cart', data);
+
     return false;
   }); // #login click end
 
